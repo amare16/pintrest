05a1cef772bffb1b3d6dcfbc80e23dbb357b45e2
amare16 <yayne16@gmail.com>
2021-09-05 09:27:48 +0200

    Add php.ini (PHP config), make Pin entity timestampable, Listing on index by createdAt desc

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -16,7 +16,7 @@ class PinsController extends AbstractController
     public function index(PinRepository $pinRepository): Response
     {
         // find all result from repository
-        $pins = $pinRepository->findAll();
+        $pins = $pinRepository->findBy([], ['createdAt' => 'DESC']);
 
         // return the result on the browser by using twig
         return $this->render('pins/index.html.twig', compact('pins'));
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
  * @ORM\Table(name="pins")
+ * @ORM\HasLifecycleCallbacks
  */
 class Pin
 {
@@ -27,6 +28,16 @@ class Pin
      * @ORM\Column(type="text")
      */
     private $description;
+
+    /**
+     * @ORM\Column(type="datetime_immutable", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -55,5 +66,43 @@ class Pin
         $this->description = $description;
 
         return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateTimestamps()
+    {
+        if ($this->getCreatedAt() === null) {
+            // we can't update this value
+            $this->setCreatedAt(new \DateTimeImmutable());
+        }
+        // only update this value
+        $this->setUpdatedAt(new \DateTimeImmutable());
     }
 }
